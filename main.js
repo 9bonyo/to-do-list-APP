a15525ceca9ab8eb467982c9fd235d4bcae547a6
bonyoung ku <byku999999999@gmail.com>
2024-05-07 18:36:41 +0900

    end

--- main.js
+++ main.js
@@ -7,13 +7,49 @@
 //3. false면 안끝난걸로 간주하고 그대로
 //진행중 끝남 탭을 누르면, 언더바 이동
 // 전체탭을 누르면 다시 전체아이템으로 돌아옴
+// + input칸에 값 입력하면, 비우기
+// + input칸에 enter만 눌러도 list에 추가되기
 
 let taskInput = document.getElementById("task-input");
+console.log("taskInput은",taskInput);
 let addButton = document.getElementById("add-button");
+let tabUnderLine = document.getElementById("under-line");
+let tabs = document.querySelectorAll(".task-tabs div");
+let mode = 'all';
+console.log(tabs);
 let taskList = [];
+let filterList = [];
+let list=[];
 
 addButton.addEventListener("click", addTask);
 
+document.addEventListener
+('keydown',
+(event)=>{if(event.keyCode === 13)
+    {console.log("enter",event);
+    if(taskInput.value ==""){
+        alert("값을 입력하세요");
+        return;
+    }else{
+    addTask();
+    taskInput.value = "";}
+    
+}
+})
+
+
+for(let i=1;i<tabs.length;i++){
+    tabs[i].addEventListener("click", function(event){filter(event), activeTab(event)}); //event는 어떤값을 클릭했는지 값
+}
+
+
+
+
+
+function activeTab(event){
+    tabUnderLine.style.left = event.currentTarget.offsetLeft + "px";
+    tabUnderLine.style.width = event.currentTarget.offsetWidth + "px";
+}
 
 function addTask(){
     
@@ -31,22 +67,36 @@ function addTask(){
 
 function render(){
     let resultHTML = '';
-    for(let i=0;i<taskList.length;i++){
-        if(taskList[i].isComplete == true){
+    
+    //내가 선택한 탭에 따라서 리스트를 달리보여주기
+    //탭 모두 클릭시 > taskList
+    //탭 진행중, 끝남 클릭시 > filterList
+    if(mode =='all'){
+            list = taskList
+    }else if(mode =='ongoing' || mode == 'done'){
+            list = filterList
+    }
+
+    console.log(filterList)
+
+
+
+    for(let i=0;i<list.length;i++){
+        if(list[i].isComplete == true){
             resultHTML += `<div class="task">
-                <div class="task-done">${taskList[i].taskContent}</div>
+                <div class="task-done">${list[i].taskContent}</div>
                     <div>
-                        <i class="fa-solid fa-check" onclick="checkTask('${taskList[i].id}')"></i>
-                        <i class="fa-solid fa-trash-can" onclick="deleteTask(${taskList[i].id})"></i>
+                        <i class="fa-solid fa-check" onclick="checkTask('${list[i].id}')"></i>
+                        <i class="fa-solid fa-trash-can" onclick="deleteTask('${list[i].id}')"></i>
                     </div>
                </div>`; 
         }
         else {
             resultHTML += `<div class="task">
-                <div>${taskList[i].taskContent}</div>
+                <div>${list[i].taskContent}</div>
                     <div>
-                        <i class="fa-solid fa-check" onclick="checkTask('${taskList[i].id}')"></i>
-                        <i class="fa-solid fa-trash-can" onclick="deleteTask('${taskList[i].id}')"></i>
+                        <i class="fa-solid fa-check" onclick="checkTask('${list[i].id}')"></i>
+                        <i class="fa-solid fa-trash-can" onclick="deleteTask('${list[i].id}')"></i>
                     </div>
                </div>`;        
         }
@@ -68,17 +118,75 @@ function checkTask(id){
    console.log(taskList);
 }
 
+function filter(event){
+    //해당 탭을 클릭했을때의 내용만 보이기
+    //underline 해당 탭 
+
+    mode = event.target.id
+    filterList =[]
+    
+    //id=all 전체리스트 > 전체
+    //id=ongoing 진행중 > isComplete == false
+    //id=done 끝남 > isComplete == true
+
+    if(mode === "all"){
+         
+        list = taskList
+        render();
+    }else if(mode === "ongoing"){
+        
+        for(let i=0;i<taskList.length;i++){
+            if(taskList[i].isComplete === false){
+                filterList.push(taskList[i]);
+            }
+        }
+        render();
+        console.log("진행중리스트", filterList);
+        
+
+
+    }else if(mode === "done"){
+        for(let i=0;i<taskList.length;i++){
+            if(taskList[i].isComplete == true){
+                filterList.push(taskList[i]);
+            }
+        }
+        render();
+        console.log("끝남리스트", filterList);
+    }
+}
+
+
 function deleteTask(id){
     console.log("삭제버튼테스트요 삭제버튼 숙제요");
-    for(let i=0; i<taskList.length;i++){
-    if(taskList[i].id == id){
-        console.log(id);
-        taskList.splice(i,1);
-        console.log(taskList);
-        break;
+    console.log("mode는",mode)
+    //만약 탭이 모두면 모두에서 삭제
+    //만약 탭이 진행중이면 진행중에서 삭제
+    //만약 탭이 끝남이면 끝남에서 삭제
+    if(mode == "all"){
+        for(let i=0; i<taskList.length;i++){
+            if(list[i].id == id){
+                taskList.splice(i,1);
+                break;
+            }
+            }
+            render();
     }
+    else if(mode=="done"||mode=="ongoing"){
+        for(let i=0; i<filterList.length;i++){
+            if(list[i].id == id){
+                filterList.splice(i,1);
+                let taskListIndex = taskList.findIndex(task => task.id === `${id}`) //**해결포인트 */
+                console.log(taskListIndex)
+                taskList.splice(taskListIndex,1);
+                break;
+            }
+            }
+            render();
     }
-    render();
+
+
+    
     console.log(taskList);
 }
 
